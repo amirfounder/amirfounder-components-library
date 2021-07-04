10398f257ef4419376d897e0d4ebec2dfec94fd0
Amir Sharapov <amirfounder18@gmail.com>
2021-07-04 09:12:01 -0500

    menu padding available. margin is availabel as spaceBetween

--- src/components/Menu/MenuStyles.js
+++ src/components/Menu/MenuStyles.js
@@ -1,23 +1,28 @@
 import styled from 'styled-components';
 import {
   generateMenuChildDisplay,
-  generateMenuChildMargin,
+  generateMenuDirectChildMargin,
+  generateMenuDirectChildPadding,
   generateMenuJustifySelf
 } from './MenuStylingService';
 
 export const MenuDiv = styled.div`
   justify-self: ${(props) => generateMenuJustifySelf(props)};
-  & > :not(first-child):not(last-child) {
-    margin: ${(props) => generateMenuChildMargin(props, 'not-first-not-last')};
-  }
-  & > :first-child {
-    margin: ${(props) => generateMenuChildMargin(props, 'first')};
-  }
-  & > :last-child {
-    margin: ${(props) => generateMenuChildMargin(props, 'last')};
-  }
 `
 
 export const MenuChildDiv = styled.div`
   display: ${(props) => generateMenuChildDisplay(props)};
+  padding: ${(props) => generateMenuDirectChildPadding(props)};
+  :only-child {
+    margin: ${(props) => generateMenuDirectChildMargin(props, 'only')};
+  }
+  :last-child {
+    margin: ${(props) => generateMenuDirectChildMargin(props, 'last')};
+  }
+  :first-child {
+    margin: ${(props) => generateMenuDirectChildMargin(props, 'first')};
+  }
+  :not(first-child):not(last-child) {
+    margin: ${(props) => generateMenuDirectChildMargin(props, 'not-first-not-last')};
+  }
 `
--- src/components/Menu/MenuStylingService.js
+++ src/components/Menu/MenuStylingService.js
@@ -7,7 +7,7 @@ export const generateMenuChildDisplay = (props) => {
   if (direction) {
     switch (direction) {
       case 'vertical': return 'block'
-      case 'horizonta':
+      case 'horizontal':
       default: return 'inline'
     }
   }
@@ -18,7 +18,7 @@ export const generateMenuChildDisplay = (props) => {
     global.direction
   ) switch (global.direction.toLowerCase()) {
     case 'vertical': return 'block'
-    case 'horizonta':
+    case 'horizontal':
     default: return 'inline'
   }
 
@@ -26,69 +26,95 @@ export const generateMenuChildDisplay = (props) => {
   return 'inline'
 }
 
-export const generateMenuChildMargin = (props, childSelector) => {
+export const generateMenuDirectChildPadding = (props) => {
   // SETUP
-  const alignment = props.alignment && props.alignment.toLowerCase();
-  const direction = props.direction && props.direction.toLowerCase();
-  const spacing = props.spacing && props.spacing.toLowerCase();
-  const children = props.children;
+  const padding = props.padding && props.padding.toLowerCase();
   const global = props.global && props.global.menu
 
-  // VALIDATE CHILDREN LENGTH
-  if (children[1].length >= 2) {
-    
-    // VALIDATE DIRECTION
-    if (direction === 'horizontal') {
+  // IMPORTANCE LEVEL 1 --> EXACT
+  if (padding) return padding;
 
-      switch (childSelector) {
+  // IMPORTANCE LEVEL 2 --> GLOBALS
+  if (
+    global &&
+    global.padding
+  ) return global.padding.toLowerCase();
 
-        // IF CHILD ELEMENT IS NOT FIRST NOR LAST
-        case 'not-first-not-last':
-          // IMPORTANCE LEVEL 1 --> EXACT
-          if (spacing) return `0px ${spacing}`
-          // IMPORTANCE LEVEL 2 --> GLOBAL
-          if (global && global.spacing) return `0px ${global.spacing}` 
-          return '0px 1.5vw';
+  // DEFAULT
+  return '0px'
+}
 
-        // IF CHILD ELEMENT IS FIRST
-        case 'first':
-          // IMPORTANCE LEVEL 1 --> EXACT
-          if (spacing) return `0px ${spacing} 0px 0px`
-          // IMPORTANCE LEVEL 2 --> GLOBAL
-          if (global && global.spacing) return `0px ${global.spacing} 0px 0px`
-          return '0px 1.5vw 0px 0px';
+export const generateMenuDirectChildMargin = (props, childSelector) => {
+  // SETUP
+  const direction = props.direction && props.direction.toLowerCase();
+  const spaceBetween = props.spaceBetween && props.spaceBetween.toLowerCase();
+  const global = props.global && props.global.menu
 
-        // IF CHILD ELEMENT IS LAST
-        case 'last':
-          // IMPORTANCE LEVEL 1 --> EXACT
-          if (spacing) return `0px 0px 0px ${spacing}`
-          // IMPORTANCE LEVEL 2 --> GLOBAL
-          if (global && global.spacing) return `0px 0px 0px ${global.spacing}`
-          return '0px 0px 0px 1.5vw';
-        
-        // ELSE --> ASSUME FIRST
-        default:
-        return '0px 1.5vw 0px 0px'
-      }
-    }
+  // IF HORIZONTAL
+  if (
+    !direction ||
+    direction === 'horizontal'
+  ) switch (childSelector) {
+
+    // IF CHILD IS ONLY CHILD ELEMENT
+    case 'only':
+      return '0vw';
+
+    // IF CHILD ELEMENT IS NOT FIRST NOR LAST
+    case 'not-first-not-last':
+      // IMPORTANCE LEVEL 1 --> EXACT
+      if (spaceBetween) return `0px ${spaceBetween}`
+      // IMPORTANCE LEVEL 2 --> GLOBAL
+      if (global && global.spaceBetween) return `0px ${global.spaceBetween}`
+      return '0px 1.5vw';
+
+    // IF CHILD ELEMENT IS FIRST
+    case 'first':
+      // IMPORTANCE LEVEL 1 --> EXACT
+      if (spaceBetween) return `0px ${spaceBetween} 0px 0px`
+      // IMPORTANCE LEVEL 2 --> GLOBAL
+      if (global && global.spaceBetween) return `0px ${global.spaceBetween} 0px 0px`
+      return '0px 1.5vw 0px 0px';
+
+    // IF CHILD ELEMENT IS LAST
+    case 'last':
+      // IMPORTANCE LEVEL 1 --> EXACT
+      if (spaceBetween) return `0px 0px 0px ${spaceBetween}`
+      // IMPORTANCE LEVEL 2 --> GLOBAL
+      if (global && global.spaceBetween) return `0px 0px 0px ${global.spaceBetween}`
+      return '0px 0px 0px 1.5vw';
+
+    // ELSE --> ASSUME ONLY ELEMENT
+    default:
+      return '0vw'
   }
 
+  // DEFAULT
   return '0vw'
 }
 
 export const generateMenuJustifySelf = (props) => {
   // SETUP
   const alignment = props.alignment && props.alignment.toLowerCase();
+  const global = props.global && props.global.toLowerCase();
 
   // IMPORTANCE LEVEL 1 --> EXACT
   if (alignment) {
     switch (alignment) {
+      // IF START OR LEFT
       case 'start':
-      case 'left': return 'start';
-      case 'center': return 'center';
+      case 'left':
+        return 'start';
+      // IF CENTER
+      case 'center':
+        return 'center';
+      // IF END OR RIGHT
       case 'end':
-      case 'right': return 'end'
-      default: return 'start'
+      case 'right':
+        return 'end'
+      // ELSE ASSUME START
+      default:
+        return 'start'
     }
   }
 
@@ -97,12 +123,20 @@ export const generateMenuJustifySelf = (props) => {
     global &&
     global.alignment
   ) switch (global.alignment) {
+    // IF START OR LEFT
     case 'start':
-    case 'left': return 'start';
-    case 'center': return 'center';
+    case 'left':
+      return 'start';
+    // IF CENTER
+    case 'center':
+      return 'center';
+    // IF END OR RIGHT
     case 'end':
-    case 'right': return 'end'
-    default: return 'start'
+    case 'right':
+      return 'end'
+    // ELSE ASSUME START
+    default:
+      return 'start'
   }
 
   // DEFAULT
